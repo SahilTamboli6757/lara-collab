f2dda3ae9a8b4da3ade3dc1e1e76724fa872253f
Sahil Tamboli <sahil@omunim.in>
2025-03-18 10:12:14 +0530

    login page update

--- resources/js/pages/Auth/Login.jsx
+++ resources/js/pages/Auth/Login.jsx
@@ -49,7 +49,7 @@ const Login = ({ notify }) => {
 
       <form onSubmit={submit}>
         <ContainerBox shadow="md" p={30} mt={30} radius="md">
-          <Group grow mb="md" mt="md">
+          {/* <Group grow mb="md" mt="md">
             <Button
               leftSection={<GoogleIcon />}
               variant="default"
@@ -61,7 +61,7 @@ const Login = ({ notify }) => {
             >
               Google
             </Button>
-          </Group>
+          </Group> */}
 
           <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
